bca001b8ed123197524f1deaaa8dee30492d74eb
Raymond Nucuta <rnucuta@gmail.com>
2019-01-02 20:19:31 -0700

    favicon

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var app = express();
 
+//set port
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
+
+// routes
 app.get('/', function(req, res) {
 	res.render('index');
 })
